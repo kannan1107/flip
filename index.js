47d4be14f1bd576ce13708d86f495eb46e7cbc0b
kannan1107 <kannan11071985@gmail.com>
2025-07-26 12:31:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let lockBoard = false;
 let matches = 0;
 let cardArray = [...cards,...cards];
 cardArray.sort(()=>0.5-Math.random());
-const gameBoard = document.querySelector(".game");
+const gameBoard = document.querySelector(".memory-game");
 
 function createCard(card) {
   const cardElement = document.createElement("div");
